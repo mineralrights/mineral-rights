5fc948e31640ac4548a2c6d6122d1ea6d466182b
LauraGomezjurado <laura.gomezjurado1@gmail.com>
2025-09-13 18:31:30 -0700

    fixed connection

--- api/app.py
+++ api/app.py
@@ -311,4 +311,5 @@ async def test():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
